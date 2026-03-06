760cd15245e7ebc9f8c126af17894b667b93346c
Sim-hub22 <np03cs4s240116@heraldcollege.edu.np>
2026-03-06 04:19:00 +0000

    ✨add eslint and prettier configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import nodePlugin from "eslint-plugin-n";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    plugins: {
+      n: nodePlugin,
+    },
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "n/no-process-env": ["error"],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
